5b6b6313b642a506731e48577977cde2c9960e38
Selvendiran Jayaraman <selvendiranj@gmail.com>
2018-05-23 22:40:16 +0530

    ML samples initial commit

---
+++ IrisPrediction/IrisPredictionResult.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IrisPrediction
+{
+    // IrisPrediction is the result returned from prediction operations
+    public class IrisPredictionResult
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+}
